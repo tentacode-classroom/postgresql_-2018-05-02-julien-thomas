87b9065d1af409e47c8c07f7d73093423e0b686a
Tom Flitterman <thomas.flitterman@ynov.com>
2019-06-03 16:13:05 +0200

    Update setup_postgresql.sh

--- scripts/setup_postgresql.sh
+++ scripts/setup_postgresql.sh
@@ -4,12 +4,10 @@
 sudo apt update && sudo apt upgrade && sudo apt full-upgrade && sudo apt autoremove
 
 # Add the repository in sources
-echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" |
-sudo tee --append /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
 
 # Import the repository signing key
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc |
-sudo apt-key add
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add
 
 # Update the package lists
 sudo apt-get update
