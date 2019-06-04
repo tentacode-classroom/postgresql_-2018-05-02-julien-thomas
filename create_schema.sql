1b31f33837f19336b4fb8aa0373be6c32d111c97
Vagrant Default User <vagrant@stretch.localdomain>
2019-06-04 11:00:09 +0000

    Update SQL schema script

--- create_schema.sql
+++ create_schema.sql
@@ -1,2 +1,20 @@
 DROP DATABASE IF EXISTS github;
 CREATE DATABASE github;
+
+\c github;
+
+CREATE TABLE IF NOT EXISTS event (
+	id bigint NOT NULL,
+	actor bigint NOT NULL,
+	repo bigint NOT NULL,
+	public boolean NOT NULL,
+	created_at varchar(100) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS actor (
+	id bigint NOT NULL,
+	login varchar(45) NOT NULL,
+	display_login varchar(45) NOT NULL,
+	url varchar(255) NOT NULL,
+	avatar_url varchar(255) NOT NULL
+)
