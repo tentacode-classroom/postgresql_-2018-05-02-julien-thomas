ea72caae375e5eb0225b5c952a69b18694c57461
Vagrant Default User <vagrant@stretch.localdomain>
2019-06-04 14:42:29 +0000

    Added the script to create the database schema

--- create_schema.sql
+++ create_schema.sql
@@ -3,18 +3,40 @@ CREATE DATABASE github;
 
 \c github;
 
-CREATE TABLE IF NOT EXISTS event (
-	id bigint NOT NULL,
-	actor bigint NOT NULL,
-	repo bigint NOT NULL,
-	public boolean NOT NULL,
-	created_at varchar(100) NOT NULL
-)
+DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS actor;
+DROP TABLE IF EXISTS repo;
+DROP TABLE IF EXISTS issue_event;
+DROP TABLE IF EXISTS push_event;
 
-CREATE TABLE IF NOT EXISTS actor (
-	id bigint NOT NULL,
+
+CREATE TABLE actor (
+	id bigint PRIMARY KEY NOT NULL,
 	login varchar(45) NOT NULL,
 	display_login varchar(45) NOT NULL,
 	url varchar(255) NOT NULL,
 	avatar_url varchar(255) NOT NULL
-)
+);
+
+CREATE TABLE repo (
+	id bigint PRIMARY KEY NOT NULL,
+	name varchar(100) NOT NULL,
+	url varchar(255) NOT NULL
+);
+
+
+CREATE TABLE event (
+	id bigint PRIMARY KEY NOT NULL,
+	actor bigint REFERENCES actor(id) NOT NULL,
+	repo bigint REFERENCES repo(id) NOT NULL,
+	public boolean NOT NULL,
+	created_at varchar(100) NOT NULL
+);
+
+CREATE TABLE issue_event (
+	issue_url varchar(255) NOT NULL
+) INHERITS (event);
+
+CREATE TABLE push_event (
+	nb_commits int NOT NULL
+) INHERITS (event);
