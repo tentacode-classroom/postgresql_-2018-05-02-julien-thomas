f735f3cff1d2b92ee5f334008d7b04acf10dd38a
Vagrant Default User <vagrant@stretch.localdomain>
2019-06-04 09:13:25 +0000

    Finished changing the settings

--- update_conf.sql
+++ update_conf.sql
@@ -1,3 +1,4 @@
 ALTER SYSTEM SET port = 1234;
+SET work_mem = '10MB';
 SELECT pg_reload_conf();
-SELECT name, setting FROM pg_settings WHERE name='port';
+SELECT name, setting FROM pg_settings WHERE name='port' OR name='work_mem';
