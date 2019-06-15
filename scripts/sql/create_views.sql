e48f4e501367f554afaa668134c2b67b898649df
tofl <thomas.flitterman@ynov.com>
2019-06-15 14:29:16 +0000

    Created new view

--- scripts/sql/create_views.sql
+++ scripts/sql/create_views.sql
@@ -5,8 +5,16 @@
 CREATE OR REPLACE VIEW nb_repos AS
 SELECT COUNT(DISTINCT repo_id) FROM push_event;
 
+-- Count the total sum of all the commits in the push_event table
+CREATE OR REPLACE VIEW nb_total_commits AS
+SELECT SUM(nb_commits) FROM push_event;
+
 
 -- Display the newly created views
 
 \echo Nombre de repositories dans push_event :
 SELECT * FROM nb_repos;
+
+\echo Somme totale des commits dans push_event :
+SELECT * FROM nb_total_commits;
+
