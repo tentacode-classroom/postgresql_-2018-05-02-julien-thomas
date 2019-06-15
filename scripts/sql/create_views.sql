e0f756f350036797700786b67d61a4ad23a478ea
tofl <thomas.flitterman@ynov.com>
2019-06-15 16:14:44 +0000

    Finished creating the views

--- scripts/sql/create_views.sql
+++ scripts/sql/create_views.sql
@@ -1,20 +1,35 @@
 
 \c github
 
--- Count the number of distinct repositories in the push_event table
-CREATE OR REPLACE VIEW nb_repos AS
-SELECT COUNT(DISTINCT repo_id) FROM push_event;
+DROP VIEW IF EXISTS push_events_statistics;
+DROP VIEW IF EXISTS issue_events_statistics;
 
--- Count the total sum of all the commits in the push_event table
-CREATE OR REPLACE VIEW nb_total_commits AS
-SELECT SUM(nb_commits) FROM push_event;
+-- Create a view containing all sorts of statistics related to the push_events table : 
+
+CREATE VIEW push_events_statistics AS
+SELECT
+	COUNT(DISTINCT repo_id) AS "Number of repositories", 
+	SUM(nb_commits) AS "Total number of commits",
+	AVG(nb_commits) AS "Average number of commits per push event"
+FROM push_event;
+
+-- Create a view containing all sorts of statistics related to the issues_events table :
+CREATE VIEW issue_events_statistics AS
+SELECT
+	MAX(count_list_repo_id.count) AS "Maximum number of issues for one repository"
+FROM
+(
+	SELECT 
+		COUNT(*) AS count
+	FROM issue_event 
+	GROUP BY repo_id
+) count_list_repo_id;
 
 
 -- Display the newly created views
 
-\echo Nombre de repositories dans push_event :
-SELECT * FROM nb_repos;
+\echo Statistiques des push_events :
+SELECT * FROM push_events_statistics;
 
-\echo Somme totale des commits dans push_event :
-SELECT * FROM nb_total_commits;
-
+\echo Statistiques des issue_events :
+SELECT * FROM issue_events_statistics;
