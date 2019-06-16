e5f8ff3ce35f83f1b852b527a53391ee16342e05
Julien <julien.seixas@free.fr>
2019-06-16 05:30:31 +0200

    optimize update conf sql

--- scripts/sql/update_conf.sql
+++ scripts/sql/update_conf.sql
@@ -1,3 +1,6 @@
-ALTER SYSTEM SET port = 1234;
-ALTER SYSTEM SET work_mem = '512MB';
+DECLARE @PORT INT = 1234
+DECLARE @MEM nvarchar(6) = '512MB'
+
+ALTER SYSTEM SET port = @PORT;
+ALTER SYSTEM SET work_mem = @MEM;
 SELECT pg_reload_conf();
