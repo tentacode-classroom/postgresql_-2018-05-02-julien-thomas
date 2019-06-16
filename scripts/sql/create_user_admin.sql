0931bfb7a3854d5423c4f4d4fa400a14c56d9de2
Julien <julien.seixas@free.fr>
2019-06-16 06:42:17 +0200

    add 'drop' to create_user sql scripts

--- scripts/sql/create_user_admin.sql
+++ scripts/sql/create_user_admin.sql
@@ -1,3 +1,5 @@
 -- Create the super administrator :
+DROP ROLE IF EXISTS super_admin;
 CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
+DROP DATABASE IF EXISTS super_admin;
 CREATE DATABASE super_admin OWNER super_admin;
--- scripts/sql/create_user_github.sql
+++ scripts/sql/create_user_github.sql
@@ -1,3 +1,5 @@
 -- Create the github user :
+DROP ROLE IF EXISTS github;
 CREATE ROLE github LOGIN PASSWORD '12345';
+DROP DATABASE IF EXISTS github;
 ALTER DATABASE github OWNER TO github;
