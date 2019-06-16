c17f134f081659787d746688ec1aaefed5e9eede
Julien <julien.seixas@free.fr>
2019-06-16 06:47:13 +0200

    change drop order in create_user sql scripts files

--- scripts/sql/create_user_admin.sql
+++ scripts/sql/create_user_admin.sql
@@ -1,6 +1,6 @@
 -- Create the super administrator :
-DROP ROLE IF EXISTS super_admin;
 DROP DATABASE IF EXISTS super_admin;
+DROP ROLE IF EXISTS super_admin;
 
 CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
 CREATE DATABASE super_admin OWNER super_admin;
