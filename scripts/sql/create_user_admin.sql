0d2560eb6d8da43dc475bab92f2b275c7dcf1923
Julien <julien.seixas@free.fr>
2019-06-16 06:46:09 +0200

    change drop order in create_user sql scripts files

--- scripts/sql/create_user_admin.sql
+++ scripts/sql/create_user_admin.sql
@@ -1,5 +1,6 @@
 -- Create the super administrator :
 DROP ROLE IF EXISTS super_admin;
-CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
 DROP DATABASE IF EXISTS super_admin;
+
+CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
 CREATE DATABASE super_admin OWNER super_admin;
--- scripts/sql/create_user_github.sql
+++ scripts/sql/create_user_github.sql
@@ -1,5 +1,6 @@
 -- Create the github user :
-DROP ROLE IF EXISTS github;
-CREATE ROLE github LOGIN PASSWORD '12345';
 DROP DATABASE IF EXISTS github;
+DROP ROLE IF EXISTS github;
+
+CREATE ROLE github LOGIN PASSWORD '12345';
 ALTER DATABASE github OWNER TO github;
